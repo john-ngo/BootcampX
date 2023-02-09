984993607b54f79d73be89bfc1520bb8d251fe58
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-02-08 22:26:18 -0700

    SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort = process.argv[2];
-const limit = process.argv[3];
+const cohort = process.argv[2] || 'FEB';
+const limit = process.argv[3] || 5;
+const values = [`%${cohort}%`, limit];
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-  })
-});
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      onsole.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    })
+  });
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort = process.argv[2];
-
-pool.query(`
+const cohort = [process.argv[2] || 'JUL02'];
+const query = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohort || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+`;
+
+pool.query(query, cohort)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
+  });
